1cba4fe0ae33124e22e245cdb48a45b977778fe6
Harshit Shukla <shuklaharshit090@gmail.com>
2026-08-06 10:40:40 +0530

    Time: 1 ms (79.76%) | Memory: 43.7 MB (63.51%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -14,45 +14,29 @@
  * }
  */
 class Solution {
-    public void nthlevel(List<Integer>a,TreeNode root,int n)
-    {
-        if(root==null) return;
-        if(n==0){
-            a.add(root.val);
-            return;
-        }
-        nthlevel(a,root.left,n-1);
-        nthlevel(a,root.right,n-1);
-    }
-    public void nthlevel2(List<Integer>a,TreeNode root,int n)
-    {
-        if(root==null) return;
-        if(n==0){
-            a.add(root.val);
-            return;
-        }
-        nthlevel2(a,root.right,n-1);
-        nthlevel2(a,root.left,n-1);
-    }
-    public int height(TreeNode root)
-    {
-        if(root==null) return 0;
-        return 1+Math.max(height(root.left),height(root.right));
-    }
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> ans=new ArrayList<>();
-        int level=height(root);
-        for(int i=0;i<level;i++)
+        List<List<Integer>>ans=new ArrayList<>();
+        if(root==null) return ans;
+        Queue<TreeNode>qt=new LinkedList<>();
+        qt.offer(root);
+        int c=0;
+        while(!qt.isEmpty())
         {
-            List<Integer>a=new ArrayList<>();
-            if(i%2==0)
+            int size=qt.size();  
+            List<Integer>a=new ArrayList<>();  
+            for(int i=0;i<size;i++)
             {
-                nthlevel(a,root,i);
+                TreeNode temp=qt.poll();
+                a.add(temp.val);
+                if(temp.left!=null) qt.offer(temp.left);
+                if(temp.right!=null) qt.offer(temp.right);
             }
-            else{
-                nthlevel2(a,root,i);
+            if(c%2!=0)
+            {
+                Collections.reverse(a);
             }
             ans.add(a);
+            c++;
         }
         return ans;
     }
