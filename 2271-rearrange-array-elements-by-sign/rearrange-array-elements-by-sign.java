d192e75b82c6fb926c4382992282c7ecb505ed07
Harshit Shukla <shuklaharshit090@gmail.com>
2026-09-05 17:32:03 +0530

    Time: 4 ms (40.87%) | Memory: 175.5 MB (57.70%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,31 +1,20 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        ArrayList<Integer>setp=new ArrayList<>();
-        ArrayList<Integer>setn=new ArrayList<>();
+        int ans[]=new int[nums.length];
+        int even=0;
+        int odd=1;
         for(int i=0;i<nums.length;i++)
         {
-            if(nums[i]<0)
+            if(even<ans.length && nums[i]>=0)
             {
-                setn.add(nums[i]);
+                ans[even]=nums[i];
+                even=even+2;
             }
-            else{
-                setp.add(nums[i]);
+            else if(odd<ans.length && nums[i]<0){
+                ans[odd]=nums[i];
+                odd=odd+2;
             }
-        }
-        int ans[]=new int[nums.length];
-        int k1=0;
-        int k2=1;
-        for(int idx:setp)
-        {
-          ans[k1]=idx;
-          k1=k1+2;
-        }
-        for(int id:setn)
-        {
-            ans[k2]=id;
-            k2=k2+2;
         }
         return ans;
     }
-
 }
