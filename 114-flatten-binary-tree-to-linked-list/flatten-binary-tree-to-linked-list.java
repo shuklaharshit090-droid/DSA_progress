3d1bbf866b41155d85ce50f9eb8fdf9ee6eddf21
Harshit Shukla <shuklaharshit090@gmail.com>
2026-09-10 21:57:10 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (52.30%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -14,23 +14,23 @@
  * }
  */
 class Solution {
-    public void dfs(ArrayList<TreeNode>ans,TreeNode root)
-    {
-        if(root==null) return;
-        ans.add(root);
-        dfs(ans,root.left);
-        dfs(ans,root.right);
-    }
     public void flatten(TreeNode root) {
-        ArrayList<TreeNode>ans=new ArrayList<>();
         if(root==null) return;
-         dfs(ans,root);
-        for(int i=0;i<ans.size()-1;i++)
+        TreeNode curr=root;
+        while(curr!=null)
         {
-          ans.get(i).right=ans.get(i+1);
-          ans.get(i).left=null;
+            if(curr.left!=null)
+            {
+                TreeNode pred=curr.left;
+                while(pred.right!=null)
+                {
+                    pred=pred.right;
+                }
+                pred.right=curr.right;
+                curr.right=curr.left;
+                curr.left=null;
+            }
+            curr=curr.right;
         }
-        ans.get(ans.size()-1).right=null;
-        ans.get(ans.size()-1).left=null;
     }
 }
