4ad34561cea692cfb68b7ed4ae5a09bfca1d734c
Harshit Shukla <shuklaharshit090@gmail.com>
2026-09-18 11:41:55 +0530

    Time: 15 ms (5.36%) | Memory: 48.7 MB (5.64%) - LeetSync

---
+++ 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumEvenGrandparent(TreeNode root) {
+       if(root==null) return 0;
+       HashMap<TreeNode,TreeNode>map=new HashMap<>();
+       Queue<TreeNode>qt=new LinkedList<>();
+       qt.offer(root);
+       while(!qt.isEmpty())
+       {
+        TreeNode temp=qt.poll();
+        if(temp.left!=null && temp.left.left!=null && temp.val%2==0) map.put(temp.left.left,temp);
+        if( temp.left!=null && temp.left.right!=null && temp.val%2==0) map.put(temp.left.right,temp);
+        if( temp.right!=null && temp.right.left!=null && temp.val%2==0) map.put(temp.right.left,temp);
+        if( temp.right!=null && temp.right.right!=null && temp.val%2==0) map.put(temp.right.right,temp);
+        if(temp.left!=null) qt.offer(temp.left);
+        if(temp.right!=null) qt.offer(temp.right);
+       }
+       int count=0;
+       for(TreeNode t:map.keySet())
+       {
+        count+=t.val;
+       }
+       return count;
+    }
+}
