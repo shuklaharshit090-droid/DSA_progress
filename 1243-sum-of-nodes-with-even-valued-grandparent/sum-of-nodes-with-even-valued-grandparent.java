e168134aae733008dbe26778072b2c7c4680305b
Harshit Shukla <shuklaharshit090@gmail.com>
2026-09-18 11:44:43 +0530

    Time: 10 ms (8.67%) | Memory: 47.2 MB (97.25%) - LeetSync

--- 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.java
+++ 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.java
@@ -16,24 +16,25 @@
 class Solution {
     public int sumEvenGrandparent(TreeNode root) {
        if(root==null) return 0;
-       HashMap<TreeNode,TreeNode>map=new HashMap<>();
+    //    HashMap<TreeNode,TreeNode>map=new HashMap<>();
+    int count=0;
        Queue<TreeNode>qt=new LinkedList<>();
        qt.offer(root);
        while(!qt.isEmpty())
        {
         TreeNode temp=qt.poll();
-        if(temp.left!=null && temp.left.left!=null && temp.val%2==0) map.put(temp.left.left,temp);
-        if( temp.left!=null && temp.left.right!=null && temp.val%2==0) map.put(temp.left.right,temp);
-        if( temp.right!=null && temp.right.left!=null && temp.val%2==0) map.put(temp.right.left,temp);
-        if( temp.right!=null && temp.right.right!=null && temp.val%2==0) map.put(temp.right.right,temp);
+        if(temp.left!=null && temp.left.left!=null && temp.val%2==0) count+=temp.left.left.val;
+        if( temp.left!=null && temp.left.right!=null && temp.val%2==0) count+=temp.left.right.val;
+        if( temp.right!=null && temp.right.left!=null && temp.val%2==0) count+=temp.right.left.val;
+        if( temp.right!=null && temp.right.right!=null && temp.val%2==0) count+=temp.right.right.val;
         if(temp.left!=null) qt.offer(temp.left);
         if(temp.right!=null) qt.offer(temp.right);
        }
-       int count=0;
-       for(TreeNode t:map.keySet())
-       {
-        count+=t.val;
-       }
+    //    int count=0;
+    //    for(TreeNode t:map.keySet())
+    //    {
+    //     count+=t.val;
+    //    }
        return count;
     }
 }
